da9ae382bcfd571799d2c541223222cca25b70c3
ph3eds <fkasler@issgs.net>
2022-12-05 23:01:33 -0500

    Force netman to give up on wlan0

--- setup/wifi_mgmt/finish_setup.sh
+++ setup/wifi_mgmt/finish_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 cp ./etc_network_interfaces.d_eth0 /etc/network/interfaces.d/eth0 
 cp ./etc_network_interfaces.d_eth1 /etc/network/interfaces.d/eth1 
+cp ./etc_NetworkManager_conf.d_99-unmanaged-devices.conf /etc/NetworkManager/conf.d/99-unmanaged-devices.conf
 systemctl enable dolos_service
